c5c1427fcb603f7f08d1c2973f1c18b62c356998
j4mehta <jeet.mehta@kik.com>
2013-04-05 12:19:50 -0400

    added code to display images, load and apply functions done, trying to figure out color keying

--- tictactoe/main.mm
+++ tictactoe/main.mm
@@ -25,6 +25,9 @@ SDL_Surface* gameGrid = NULL;
 //Font Declaration
 TTF_Font* myfont = NULL;
 
+//Event Declaration
+SDL_Event event;
+
 //Initialization Function
 bool init()
 {
@@ -34,6 +37,7 @@ bool init()
     }
     
     screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE);
+    
     if (screen == NULL)
     {
         return false;
@@ -47,10 +51,109 @@ bool init()
     return true;
 }
 
+SDL_Surface* loadImages(std:: string filename, bool colorKey)
+{
+    SDL_Surface* inputImage = NULL;
+    SDL_Surface* outputImage = NULL;
+    
+    inputImage = IMG_Load(filename.c_str());
+    if (inputImage != NULL)
+    {
+        outputImage = SDL_DisplayFormat(inputImage);
+        SDL_FreeSurface(inputImage);
+        
+        if (outputImage != NULL)
+        {
+           //Code for Color keying
+            if (colorKey == true)
+            {
+                Uint32 key = SDL_MapRGB(outputImage->format, 255, 0, 254);
+                SDL_SetColorKey(outputImage, SDL_SRCCOLORKEY, key);
+            }
+        }
+    }
+    
+    return outputImage;
+}
+
+
+bool loadFiles()
+{
+    gameGrid = loadImages("grid.png", false);
+    xToken = loadImages("x.png", true);
+    oToken = loadImages("o.png", true);
+    
+    if (gameGrid == NULL || xToken == NULL || oToken == NULL)
+    {
+        return false;
+    }
+    
+    myfont = TTF_OpenFont("stocky.TTF", 26);
+    
+    if (myfont == NULL)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+bool applySurface (int x, int y, SDL_Surface* source, SDL_Surface* dest)
+{
+    SDL_Rect offset;
+    
+    offset.x = x;
+    offset.y = y;
+    
+    if (SDL_BlitSurface(source, NULL, dest, &offset) == -1)
+    {
+        return false;
+    }
+    
+    return true;
+}
+
+void quitProgram()
+{
+    TTF_CloseFont(myfont);
+    TTF_Quit();
+    SDL_Quit();
+}
+
 int main(int argc, char ** argv)
 {
-    SDL_Quit();
-    TTF_Quit();
+    
+    bool quit = false;
+    
+    
+    if (init() == false)
+    {
+        return -1;
+    }
+    
+    if (loadFiles() == false)
+    {
+        return -1;
+    }
+    
+    applySurface(0, 0, gameGrid, screen);
+    applySurface(0, 0, xToken, screen);
+    applySurface(200, 200, oToken, screen);
+    
+    SDL_Flip(screen);
+    
+    while (quit == false)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+        }
+    }
+    
+    quitProgram();
     return 0;
 }
 
